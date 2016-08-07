5ffb964fc9240333a53c086433f8283d8c758d58
WOLFI3654 <luigi3654@gmail.com>
2016-08-07 14:24:41 +0200

    Added Constructor and some Automatic Stuff

--- Minopoly/src/de/wolfi/minopoly/components/minigames/MinigameHook.java
+++ Minopoly/src/de/wolfi/minopoly/components/minigames/MinigameHook.java
@@ -4,14 +4,20 @@ import java.util.List;
 import java.util.Vector;
 
 import de.wolfi.minopoly.components.Player;
+import de.wolfi.minopoly.utils.TeleportCause;
 
 public class MinigameHook {
-
+	
+	private final Minigame mg;
+	public MinigameHook(Minigame mg) {
+		this.mg = mg;
+	}
+	
 	private final Vector<Player> players = new Vector<>(4, 2);
 	
 	public String getName() {return "Unknown Minigame";}
 	
-	public void addPlayer(Player player){players.add(player);}
+	public void addPlayer(Player player){players.add(player); player.teleport(mg.getLocation(), TeleportCause.MINIGAME_STARTED);}
 	
 	public void removePlayer(Player player){players.remove(player);}
 	
