97feb8c5aaf5661d51dfe17b719c8c29deccc239
WOLFI3654 <luigi3654@gmail.com>
2016-08-07 14:16:07 +0200

    Minigame Hook added

---
+++ Minopoly/src/de/wolfi/minopoly/components/minigames/MinigameHook.java
@@ -0,0 +1,26 @@
+package de.wolfi.minopoly.components.minigames;
+
+import java.util.List;
+import java.util.Vector;
+
+import de.wolfi.minopoly.components.Player;
+
+public class MinigameHook {
+
+	private final Vector<Player> players = new Vector<>(4, 2);
+	
+	public String getName() {return "Unknown Minigame";}
+	
+	public void addPlayer(Player player){players.add(player);}
+	
+	public void removePlayer(Player player){players.remove(player);}
+	
+	public void start(){}
+	
+	public List<Player> getPlayers(){return players;};
+	
+	protected final void win(Player... players){
+		
+	}
+	
+}
