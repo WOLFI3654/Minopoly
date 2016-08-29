e7180c658c87e91d10b95930e7ca36f5df318995
WOLFI3654 <luigi3654@gmail.com>
2016-08-29 18:52:39 +0200

    Added Minigame Kill Messages

--- Minopoly/src/de/wolfi/minopoly/utils/Messages.java
+++ Minopoly/src/de/wolfi/minopoly/utils/Messages.java
@@ -6,6 +6,9 @@ import org.bukkit.entity.Player;
 public enum Messages {
 
 	MINIGAME_WIN("Das Minispiel $0 wurde von $1 gewonnen!"),
+	MINIGAME_DEATH("$0 ist in $1 gestorben."),
+	MINIGAME_GOT_KILLED("$0 wurde in $1 von $2 getötet!"),
+	MINIGAME_KILL("Du hast $0 in $1 getötet!"),
 	TELEPORT("Du wurdest teleportiert."),
 	OTHER_FIELD_ENTERED("$0 ist auf ein Feld von $1 getreten! §l($2)");
 	
