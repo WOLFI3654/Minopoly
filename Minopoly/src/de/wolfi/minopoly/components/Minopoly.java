a5b0642a32c60697cc1961559104e78ea1fc2ea1
WOLFI3654 <luigi3654@gmail.com>
2017-02-19 12:35:38 +0100

    Importet CommandSender

--- Minopoly/src/de/wolfi/minopoly/components/Minopoly.java
+++ Minopoly/src/de/wolfi/minopoly/components/Minopoly.java
@@ -2,16 +2,24 @@ package de.wolfi.minopoly.components;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Set;
 
 import javax.annotation.Nullable;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.World;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.HandlerList;
+import org.bukkit.permissions.Permissible;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
 
 import de.wolfi.minopoly.components.fields.Field;
 
-public class Minopoly implements Serializable{
+public class Minopoly implements Serializable, CommandSender{
 
 	/**
 	 * 
@@ -86,6 +94,106 @@ public class Minopoly implements Serializable{
 		}
 		if(!next) return null;
 		return fields.get(0);
+	}
+
+
+	
+	
+	
+	@Override
+	public PermissionAttachment addAttachment(Plugin arg0) {
+		return null;
+	}
+
+
+	@Override
+	public PermissionAttachment addAttachment(Plugin arg0, int arg1) {
+		return null;
+	}
+
+
+	@Override
+	public PermissionAttachment addAttachment(Plugin arg0, String arg1, boolean arg2) {
+		return null;
+	}
+
+
+	@Override
+	public PermissionAttachment addAttachment(Plugin arg0, String arg1, boolean arg2, int arg3) {
+		return null;
+	}
+
+
+	@Override
+	public Set<PermissionAttachmentInfo> getEffectivePermissions() {
+		return null;
+	}
+
+
+	@Override
+	public boolean hasPermission(String arg0) {
+		return true;
+	}
+
+
+	@Override
+	public boolean hasPermission(Permission arg0) {
+		return true;
+	}
+
+
+	@Override
+	public boolean isPermissionSet(String arg0) {
+		return true;
+	}
+
+
+	@Override
+	public boolean isPermissionSet(Permission arg0) {
+		return true;
+	}
+
+
+	@Override
+	public void recalculatePermissions() {}
+
+
+	@Override
+	public void removeAttachment(PermissionAttachment arg0) {}
+
+
+	@Override
+	public boolean isOp() {
+		return true;
+	}
+
+
+	@Override
+	public void setOp(boolean arg0) {}
+
+
+	@Override
+	public String getName() {
+		return toString();
+	}
+
+
+	@Override
+	public Server getServer() {
+		return Bukkit.getServer();
+	}
+
+
+	@Override
+	public void sendMessage(String arg0) {
+		Bukkit.broadcastMessage(arg0);
+	}
+
+
+	@Override
+	public void sendMessage(String[] arg0) {
+		for(String msg:arg0)Bukkit.broadcastMessage(msg);
+		
 	}
 	
 }
