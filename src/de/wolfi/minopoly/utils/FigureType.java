620e716624aab3208a4cb2075280f72258c1a503
WOLFI3654 <luigi3654@gmail.com>
2017-03-08 16:55:17 +0100

    Some editing

--- src/de/wolfi/minopoly/utils/FigureType.java
+++ src/de/wolfi/minopoly/utils/FigureType.java
@@ -1,22 +1,29 @@
 package de.wolfi.minopoly.utils;
 
 import org.bukkit.DyeColor;
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.ItemStack;
 
 import de.robingrether.idisguise.disguise.Disguise;
 import de.robingrether.idisguise.disguise.DisguiseType;
+import de.wolfi.utils.ItemBuilder;
 
 public enum FigureType {
 
-	WOLF(DisguiseBuilder.create(DisguiseType.WOLF).setCollar(DyeColor.RED).create(), EntityType.WOLF),
-	SHEEP(DisguiseBuilder.create(DisguiseType.SHEEP).setColor(DyeColor.BLUE).create(),EntityType.SHEEP);
+	WOLF("Wolf", DisguiseBuilder.create(DisguiseType.WOLF).setCollar(DyeColor.RED).create(), EntityType.WOLF, new ItemBuilder(Material.BONE).build()),
+	SHEEP("Blue Sheep", DisguiseBuilder.create(DisguiseType.SHEEP).setColor(DyeColor.BLUE).create(),EntityType.SHEEP, new ItemBuilder(Material.WOOL).dye(DyeColor.BLUE).build());
 
 	private Disguise d;
 	private EntityType entityType;
+	private ItemStack item;
+	private String name;
 
-	private FigureType(Disguise d, EntityType en) {
+	private FigureType(String name, Disguise d, EntityType en, ItemStack item) {
 		this.d = d;
 		this.entityType = en;
+		this.item = item;
+		this.name = name;
 	}
 
 	public Disguise createDisguise() {
@@ -27,7 +34,15 @@ public enum FigureType {
 		return this.entityType;
 	}
 
+	public String getDisplay(){
+		return this.name;
+	}
+	
 	public String getName() {
-		return this.d.getType().toString();
+		return this.toString();
+	}
+
+	public ItemStack getItem() {
+		return item;
 	}
 }
