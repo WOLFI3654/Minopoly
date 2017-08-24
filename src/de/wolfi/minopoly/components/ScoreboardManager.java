78f6d591eded817968ec52fd82090f2b23684f1f
WOLFI3654 <luigi3654@gmail.com>
2017-08-24 22:33:26 +0200

    Sum Scorebroat

---
+++ src/de/wolfi/minopoly/components/ScoreboardManager.java
@@ -0,0 +1,36 @@
+package de.wolfi.minopoly.components;
+
+import org.bukkit.ChatColor;
+import org.bukkit.scoreboard.DisplaySlot;
+
+import de.wolfi.utils.Messages;
+import de.wolfi.utils.Scoreboard;
+
+public class ScoreboardManager extends GameObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7018585926573683540L;
+
+	private transient Scoreboard scoreboard;
+
+	@Override
+	protected void load() {
+		this.scoreboard = new Scoreboard(Messages.Prefix, DisplaySlot.SIDEBAR);
+	}
+
+	protected void addPlayer(Player player) {
+		this.scoreboard.addPlayer(player.getHook());
+		this.updatePlayer(player);
+	}
+
+	protected void updatePlayer(Player player) {
+		this.scoreboard.setScore(ChatColor.GREEN + player.getFigure().getName(), player.getMoney());
+	}
+
+	@Override
+	protected void unload() {
+	}
+
+}
