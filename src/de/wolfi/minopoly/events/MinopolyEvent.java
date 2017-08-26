dadc2ff29ded2c9d8eec6c81361d31a45107c5bb
WOLFI3654 <luigi3654@gmail.com>
2017-08-26 17:24:49 +0200

    Made cancellable

--- src/de/wolfi/minopoly/events/MinopolyEvent.java
+++ src/de/wolfi/minopoly/events/MinopolyEvent.java
@@ -1,12 +1,14 @@
 package de.wolfi.minopoly.events;
 
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 import de.wolfi.minopoly.components.Player;
 
-public class MinopolyEvent extends Event{
+public class MinopolyEvent extends Event implements Cancellable{
 
+	private boolean cancelled = false;
 	protected Player player;
 	public MinopolyEvent(Player player) {
 		this.player = player;
@@ -21,6 +23,16 @@ public class MinopolyEvent extends Event{
 	
 	public Player getPlayer() {
 		return player;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean arg0) {
+		this.cancelled = arg0;
 	}
 
 	
