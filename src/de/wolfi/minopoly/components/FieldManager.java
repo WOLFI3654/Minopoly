a1f8c9ac30fac6bc2adbaa1d4f9a656c0df8ca4c
WOLFI3654 <luigi3654@gmail.com>
2017-08-29 12:41:34 +0200

    updatet get<Type>Field

--- src/de/wolfi/minopoly/components/FieldManager.java
+++ src/de/wolfi/minopoly/components/FieldManager.java
@@ -5,32 +5,35 @@ import java.util.HashMap;
 
 import de.wolfi.minopoly.components.fields.Field;
 import de.wolfi.minopoly.components.fields.FieldColor;
+import de.wolfi.minopoly.components.fields.JailField;
 import de.wolfi.minopoly.components.fields.StartField;
 
-public class FieldManager extends GameObject{
+public class FieldManager extends GameObject {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6097015591168469852L;
 
-	
 	private final ArrayList<Field> fields = new ArrayList<>();
-	private final HashMap<FieldColor,ArrayList<Field>> mappedList = new HashMap<>();
-	
-	protected FieldManager() {}
-	
+	private final HashMap<FieldColor, ArrayList<Field>> mappedList = new HashMap<>();
+
+	protected FieldManager() {
+	}
+
 	public Field addField(Field f) {
 		f.spawn();
 		this.fields.add(f);
-		this.addMapped(f.getColor(),f);
+		this.addMapped(f.getColor(), f);
 		return f;
 	}
-	
+
 	private void addMapped(FieldColor color, Field f) {
 		ArrayList<Field> list = this.mappedList.get(color);
-		if(list == null) list = new ArrayList<>();
-		if(!list.contains(f)) list.add(f);
+		if (list == null)
+			list = new ArrayList<>();
+		if (!list.contains(f))
+			list.add(f);
 	}
 
 	public Field getNextField(Field from) {
@@ -46,17 +49,18 @@ public class FieldManager extends GameObject{
 
 		return this.fields.get(0);
 	}
-	
+
 	@Override
-	protected void load(){
+	protected void load() {
 		for (final Field f : this.fields)
 			f.load();
 	}
-	
-	public int countProperties(Player player, FieldColor color){
+
+	public int countProperties(Player player, FieldColor color) {
 		int count = 0;
-		for(Field f : mappedList.get(color)){
-			if(f.isOwnedBy(player)) count++;
+		for (Field f : mappedList.get(color)) {
+			if (f.isOwnedBy(player))
+				count++;
 		}
 		return count;
 	}
@@ -65,17 +69,34 @@ public class FieldManager extends GameObject{
 	protected void unload() {
 		for (final Field f : this.fields)
 			f.unload();
+
+	}
+
+	public Field getFieldByType(Field start, Class<? extends Field> type) {
+		boolean found = false;
+		if (start == null)
+			found = true;
+		Field next = null;
+		for (final Field f : this.fields) {
+			if (f.getClass() == type) {
+				next = f;
+				found = true;
+				break;
+			}
+		}
+		while(found == false){
+			next = getNextField(next);
+			if(next.getClass() == type) found = true;
+		}
 		
+		return next;
 	}
 
 	public Field getStartField() {
-		Field next = null;
-		for (final Field f : this.fields) {
-			if (f instanceof StartField) {
-				next = f;
-				continue;
-			}
-		}
-		return next;
+		return this.getFieldByType(null, StartField.class);
+	}
+	
+	public Field getJailField(){
+		return this.getFieldByType(null, JailField.class);
 	}
 }
