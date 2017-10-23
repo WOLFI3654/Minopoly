4c549d9aada655ae8a1fee9fb561f249ed8c8b16
root <luigi3654@gmail.com>
2017-10-23 15:15:47 +0200

    Added block and buy protection

--- src/de/wolfi/minopoly/components/fields/PayingField.java
+++ src/de/wolfi/minopoly/components/fields/PayingField.java
@@ -1,6 +1,8 @@
 package de.wolfi.minopoly.components.fields;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.material.MaterialData;
 
 import de.wolfi.minopoly.components.Minopoly;
 import de.wolfi.minopoly.components.Player;
@@ -21,13 +23,20 @@ public class PayingField extends Field{
 		
 	}
 
+	
+	@Override
+	public boolean buy(Player player) {
+		return false;
+	}
 	@Override
 	public boolean isOwned() {
 		return false;
 	}
 	@Override
 	public void spawn() {
-		
+		System.out.println("Spawning paying");
+		this.getCircle(0, false, new MaterialData(Material.AIR));
+		this.getCircle(0, true, new MaterialData(Material.IRON_BLOCK));
 	}
 
 }
